f10d5d9f1ba8af9124b59177c3e5ac730ce095bb
kelechi-aims <kelechi@aims.edu.gh>
2023-06-22 13:55:20 +0100

    task 12 correction to error message

--- opcode_file4.c
+++ opcode_file4.c
@@ -62,7 +62,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 
 	(void)line_number;
 	while (current != NULL && current->n != 0 &&
-		current->n >= 0 && current-> <= 127)
+		current->n >= 0 && current->n <= 127)
 	{
 		printf("%c", current->n);
 		current = current->next;
